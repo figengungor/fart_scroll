09b99301aa3f2aa7209707ec9fd0de93dee59bac
Dobszai János <dobszaijani@gmail.com>
2023-08-11 16:39:05 +0200

    Farts with Flutter 3.10

--- lib/fart_scroll.dart
+++ lib/fart_scroll.dart
@@ -1,15 +1,16 @@
 library fart_scroll;
 
 import 'dart:io';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:path_provider/path_provider.dart';
 
 mixin FartScrollMixin<T extends StatefulWidget> on State<T> {
-  ScrollController _scrollController;
-  AudioPlayer _audioPlayer;
+  ScrollController? _scrollController;
+  AudioPlayer? _audioPlayer;
   double _fartOffset = 0.0;
   ScrollDirection _prevDirection = ScrollDirection.reverse;
   double _fartScrollDistance = 300.0;
@@ -20,17 +21,17 @@ mixin FartScrollMixin<T extends StatefulWidget> on State<T> {
     _audioPlayer = AudioPlayer();
     _scrollController = ScrollController();
 
-    _scrollController.addListener(() {
-      if (_scrollController.position.userScrollDirection ==
+    _scrollController?.addListener(() {
+      if (_scrollController?.position.userScrollDirection ==
           ScrollDirection.reverse) {
-        if (_fartOffset < _scrollController.offset) {
+        if (_fartOffset < _scrollController!.offset) {
           //Reverse Fart
           _fartOffset = _fartOffset + _fartScrollDistance;
           _loadFileAndPlay('fart.mp3');
           _resetOffsetIfDirectionChanged();
         }
       } else {
-        if (_fartOffset > _scrollController.offset) {
+        if (_fartOffset > _scrollController!.offset) {
           //Forward Fart
           _fartOffset = _fartOffset - _fartScrollDistance;
           _loadFileAndPlay('fart.mp3');
@@ -42,15 +43,15 @@ mixin FartScrollMixin<T extends StatefulWidget> on State<T> {
 
   @override
   void dispose() {
-    _scrollController.dispose();
-    _audioPlayer.release();
+    _scrollController?.dispose();
+    _audioPlayer?.release();
     super.dispose();
   }
 
   void _resetOffsetIfDirectionChanged() {
-    if (_prevDirection != _scrollController.position.userScrollDirection) {
-      _prevDirection = _scrollController.position.userScrollDirection;
-      _fartOffset = _scrollController.offset;
+    if (_prevDirection != _scrollController?.position.userScrollDirection) {
+      _prevDirection = _scrollController!.position.userScrollDirection;
+      _fartOffset = _scrollController!.offset;
     }
   }
 
@@ -60,8 +61,8 @@ mixin FartScrollMixin<T extends StatefulWidget> on State<T> {
         (await rootBundle.load('packages/fart_scroll/assets/$path'))
             .buffer
             .asUint8List());
-    await _audioPlayer.play(file.path, isLocal: true);
+    await _audioPlayer?.play(DeviceFileSource(file.path));
   }
 
-  ScrollController get fartScrollController => _scrollController;
+  ScrollController? get fartScrollController => _scrollController;
 }
